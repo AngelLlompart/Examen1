5e9a8a293af80af9042e90011c6ef73700a296ed
unknown <angelllompart@paucasesnovescifp.cat>
2022-11-30 11:15:55 +0100

    Created lvl 2 + A/D rotation

--- Assets/Challenge 1/Scripts/PlayerControllerX.cs
+++ Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -7,6 +7,7 @@ public class PlayerControllerX : MonoBehaviour
     public float speed;
     public float rotationSpeed;
     public float verticalInput;
+    public float horizontalInput;
 
     // Start is called before the first frame update
     void Start()
@@ -19,6 +20,9 @@ public class PlayerControllerX : MonoBehaviour
     {
         // get the user's vertical input
         verticalInput = Input.GetAxis("Vertical");
+        
+        //get the user's horizontal input
+        horizontalInput = Input.GetAxis("Horizontal");
 
         // move the plane forward at a constant rate
         transform.Translate(Vector3.forward * speed * Time.deltaTime);
@@ -31,6 +35,14 @@ public class PlayerControllerX : MonoBehaviour
         else if(verticalInput > 0)
         {
             transform.Rotate(Vector3.left * rotationSpeed * Time.deltaTime);
+        }
+
+        if (horizontalInput < 0)
+        {
+            transform.Rotate(Vector3.forward * rotationSpeed * Time.deltaTime);
+        } else if (horizontalInput > 0)
+        {
+            transform.Rotate(Vector3.back * rotationSpeed * Time.deltaTime);
         }
         
     }
